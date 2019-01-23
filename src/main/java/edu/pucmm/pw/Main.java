1b5ca4ca1ab20851d2fd7949fda216fa01f068ad
karvin <karvin_jimenez@hotmail.com>
2019-01-23 00:19:11 -0500

    Cambios pc desktop

--- src/main/java/edu/pucmm/pw/Main.java
+++ src/main/java/edu/pucmm/pw/Main.java
@@ -5,6 +5,7 @@ public class Main {
     {
         System.out.println("hola mundo");
         System.out.println("prueba de cambio");
+        System.out.println("prueba de cambio en la desktop");
 
     }
 }
