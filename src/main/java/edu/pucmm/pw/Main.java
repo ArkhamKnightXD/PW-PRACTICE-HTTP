74917c4d262d376bd430f5dbd17eec35ed5d519d
karvin <karvin_jimenez@hotmail.com>
2019-01-23 12:58:29 -0500

    Agregando import de Jsoup

--- src/main/java/edu/pucmm/pw/Main.java
+++ src/main/java/edu/pucmm/pw/Main.java
@@ -1,11 +1,25 @@
 package edu.pucmm.pw;
 
+import org.jsoup.Jsoup;
+import org.jsoup.helper.Validate;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+
 public class Main {
     public static void main(String[] args)
     {
         System.out.println("hola mundo");
-        System.out.println("prueba de cambio");
-        System.out.println("prueba de cambio en la desktop");
 
+      //  Document doc=Jsoup.connect("https://www.wikihow.com/Improve-WiFi-Reception").timeout(6000).get();
+
+      /*  Elements ele=p.select("div#reviews");
+        for (Element element: ele)
+        {
+            String img_url=element.select(div.prerendered);
+            System.out.println("img_url");
+
+        }*/
     }
 }
