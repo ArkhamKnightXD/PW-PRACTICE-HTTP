b976aa88892a7403f7a98b251ded3610804266aa
ArkhamKnightXD <39208059+ArkhamKnightXD@users.noreply.github.com>
2019-01-23 23:18:39 -0400

    Solucionados punto B y C de la practica, todavia sigo trabajando en el A y el D

--- src/main/java/edu/pucmm/pw/Main.java
+++ src/main/java/edu/pucmm/pw/Main.java
@@ -12,7 +12,50 @@ public class Main {
     {
         System.out.println("hola mundo");
 
-      //  Document doc=Jsoup.connect("https://www.wikihow.com/Improve-WiFi-Reception").timeout(6000).get();
+        final String url ="https://shares.telegraph.co.uk/indices/summary/index/MCX";
+
+        try {
+            final Document document=Jsoup.connect(url).get(); // comando para establecer la conexion con la url que queremos analizar
+
+
+          // System.out.println(document.outerHtml()); // Este comando nos imprime el codigo html de la pagina completo
+
+            //int lines = document.html().split(System.getProperty("line.separator")).length;
+           // System.out.println(lines);
+
+
+            String textoCompleto2 = document.html();
+
+            String textoCompleto = document.outerHtml();
+
+            document.getElementsByAttribute("POST").size();
+
+            document.getElementsByAttribute("GET").size();
+
+
+
+            int imagenes= document.getElementsByTag("img").size();
+
+            int parrafos= document.getElementsByTag("p").size();
+
+            document.getElementsByAttributeValue("type","text");
+
+            int cantidadElementos = document.getAllElements().size(); // Esto cuenta la cantidad de elementos de la pagina, pero no cuenta body ni expacios en blanco
+
+
+            System.out.println(textoCompleto);
+
+            System.out.println(cantidadElementos);
+
+            System.out.println(parrafos); // Este codigo permite contar cuantos elemento hay del tag que uno defina
+
+            System.out.println(imagenes);
+
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+
+
 
       /*  Elements ele=p.select("div#reviews");
         for (Element element: ele)
