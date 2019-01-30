8152d6db13245915b6db851424045ddfc7b761f4
ArkhamKnightXD <39208059+ArkhamKnightXD@users.noreply.github.com>
2019-01-30 10:58:32 -0400

    Trabajando con el punto F

--- src/main/java/edu/pucmm/pw/Main.java
+++ src/main/java/edu/pucmm/pw/Main.java
@@ -3,19 +3,26 @@ package edu.pucmm.pw;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.Connection;
+import java.util.HashMap;
+import java.util.Map;
+import java.io.IOException;
 
 
 public class Main {
     public static void main(String[] args)
     {
 
-         String url ="https://www.pabloyglesias.com/obtener-datos-publicos-persona/";
+         String url ="http://itachi.avathartech.com:4567/opcion2.html";
 
         try {
              Document document=Jsoup.connect(url).get(); // comando para establecer la conexion con la url que queremos analizar
 
+            //Punto B
             int parrafos= document.getElementsByTag("p").size();
 
+
+            //Punto C
             int[] cantidad = {0};
 
             document.getElementsByTag("p").forEach(element ->
@@ -27,6 +34,7 @@ public class Main {
             });
 
 
+            //Punto D
             int[] cantidadFormGet = {0};
 
             document.getElementsByTag("form").forEach(element -> {
@@ -46,6 +54,8 @@ public class Main {
 
             int cantidaElementos = document.getAllElements().size(); // Creo que este codigo podria ser una opcion para la respuesta A
 
+            //Punto A
+
 
             System.out.println("La cantidad totales de lineas del recurso son : " + document.html().split("\n").length);
 
@@ -59,10 +69,34 @@ public class Main {
 
             System.out.println("Los campos del tipo input de cada formulario son los siguientes: ");
 
+
+            //Punto E
             for (Element tiposForm : document.getElementsByTag("form"))
             {
                 System.out.println(tiposForm);
             }
+
+            //punto F
+
+            Connection.Response response = null;
+            Document doc = null;
+            Map<String, String> parametros = new HashMap<>();
+
+
+                for (Element element : document.getElementsByTag("form")) {
+                    String absURL = element.absUrl("action");
+
+                    if (element.attr("method").equals("post")) {
+                        parametros.put("asignatura", "practica1");
+
+                        document = Jsoup.connect(absURL)
+                                .method(Connection.Method.POST)
+                                .data(parametros)
+                                .post();
+
+                        System.out.println(document.outerHtml());
+                    }
+                }
 
 
         }catch (Exception ex){
